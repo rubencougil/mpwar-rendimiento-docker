6614698d53a5ae17845a1277fef60fda3863e41c
rubencougil <rubencougil@gmail.com>
2019-12-14 21:36:24 +0100

    elastica

--- www/html/test-app/index.php
+++ www/html/test-app/index.php
@@ -3,9 +3,12 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
+use Elastica\Client as ElasticaClient;
 
 $db = new PDO('mysql:dbname=db;host=mysql', 'user', 'password');
 $redis = (new Redis())->connect('redis');
 $connection = new AMQPStreamConnection('rabbitmq', 5672, 'rabbitmq', 'rabbitmq');
+$elastic = (new ElasticaClient(['host' => 'elasticsearch', 'port' => 9200]));
+$elastic->getVersion();
 
 echo 'Hello World!';
